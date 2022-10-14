3073dfcbe73d50620b6b0a391d892a125e2d12d2
Takhir Berdyiev <xtishka@WinXT>
2022-10-14 16:07:45 +0300

    Move to jetstream

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-require __DIR__.'/auth.php';
+    Route::get('/google-calendar', function () {
+        return view('google-calendar');
+    })->name('google-calendar');
+});
